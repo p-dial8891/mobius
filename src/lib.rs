873cf4ebd7b8d0038258bde9833845ce07088f46
Philip Thomas <philip.tms.6@gmail.com>
2025-05-05 14:50:02 +0100

    Added mobius as a rpc service.

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,10 @@ use tracing_subscriber::{fmt::format::FmtSpan, prelude::*};
 /// This is the service definition. It looks a lot like a trait definition.
 /// It defines one RPC, hello, which takes one arg, name, and returns a String.
 #[tarpc::service]
-pub trait World {
+pub trait Mobius {
     /// Returns a greeting for name.
-    async fn hello(name: String) -> String;
+    async fn username(secret: String, id: String) -> String;
+	async fn password(secret: String, id: String) -> String;
 }
 
 /// Initializes an OpenTelemetry tracing subscriber with a OTLP backend.
