9fe02a713de66116cdfb617d03d4f0d31f552e1e
hardcoder <proteus.dial8891@gmail.com>
2025-05-07 22:49:11 +0100

    Added proper keyboard codes.

--- src/server.rs
+++ src/server.rs
@@ -34,6 +34,152 @@ use serde_json::{Result, Value};
 use std::error::Error as OtherError;
 use std::io::BufReader;
 
+
+fn get_key_codes( c : char ) -> ( bool, u8 )
+{
+	let capital = c.is_ascii_uppercase();
+	match c.to_ascii_lowercase() {
+		
+	'a'    => (capital,  4),
+	
+	'b'    => (capital,  5),
+
+	'c'    => (capital,  6),
+	
+	'd'    => (capital,  7),
+
+	'e'    => (capital,  8),
+	
+	'f'    => (capital,  9),
+
+	'g'    => (capital, 10),
+	
+	'h'    => (capital, 11),
+	
+	'i'    => (capital, 12),
+	
+	'j'    => (capital, 13),
+	
+	'k'    => (capital, 14),
+	
+	'l'    => (capital, 15),
+	
+	'm'    => (capital, 16),
+	
+	'n'    => (capital, 17),
+	
+	'o'    => (capital, 18),
+	
+	'p'    => (capital, 19),
+
+	'q'    => (capital, 20),
+
+	'r'    => (capital, 21),
+	
+	's'    => (capital, 22),
+	
+	't'    => (capital, 23),
+	
+	'u'    => (capital, 24),
+
+	'v'    => (capital, 25),
+	
+	'w'    => (capital, 26),
+	
+	'x'    => (capital, 27),
+	
+	'y'    => (capital, 28),
+
+	'z'    => (capital, 29),
+
+	'0'    => (capital, 30),
+
+	')'    => (true,    39),
+	
+	'1'    => (false,   30),
+	
+	'!'    => (true,    30),
+	
+	'2'    => (false,   31),
+	
+	'@'    => (true,    31),
+
+	'3'    => (false,   32),
+	
+	'#'    => (true,    32),
+
+	'4'    => (false,   33),
+
+	'$'    => (true,    33),
+	
+	'5'    => (false,   34),
+
+	'%'    => (true,    34),
+
+	'6'    => (false,   35),
+	
+	'^'    => (true,    35),
+
+	'7'    => (false,   36),
+	
+	'&'    => (true,    36),
+
+	'8'    => (false,   37),
+	
+	'*'    => (true,    37),
+
+	'9'    => (false,   38),
+
+	'('    => (true,    38),
+	
+	'-'    => (false,   45),
+
+	'_'    => (true,    45),
+
+	'='    => (false,   46),
+
+	'+'    => (true,    46),
+
+	'['    => (false,   47),
+	
+	'{'    => (true,    47),
+	
+	']'    => (false,   48),
+
+	'}'    => (true,    48),
+
+	'\''   => (false,   49),
+
+	'|'    => (true,    49),
+
+	';'    => (false,   51),
+	
+	':'    => (true,    51),
+
+	'\''   => (false,   52),
+
+	'"'    => (true,    52),
+	
+	'`'    => (false,   53),
+
+	'~'    => (true,    53),
+
+	','    => (false,   54),
+
+	'<'    => (true,    54),
+
+	'.'    => (false,   55),
+	
+	'>'    => (true,    55),
+	
+	'/'    => (false,   56),
+
+	'?'    => (true,    56),
+	
+	 _     => (true,    56)
+	}
+}
+
 #[derive(Parser)]
 struct Flags {
     /// Sets the secret key to use
@@ -57,9 +203,9 @@ impl Mobius for MobiusServer {
         let mut gadgetFIle = File::create("/dev/hidg0").await.unwrap();
         if secret == self.2 {
             for c in self.1[id][0].as_str().expect("Unknown JSON id").chars() {
-                let cx = (c as u8) - ('a' as u8) + 4;
+                let cx = get_key_codes(c);
                 gadgetFIle
-                    .write_all(vec![0, 0, 0, cx, 0, 0, 0, 0].as_slice())
+                    .write_all(vec![if cx.0 {2} else {0}, 0, 0, cx.1, 0, 0, 0, 0].as_slice())
                     .await;
                 gadgetFIle
                     .write_all(vec![0, 0, 0, 0, 0, 0, 0, 0].as_slice())
@@ -77,9 +223,9 @@ impl Mobius for MobiusServer {
         let mut gadgetFIle = File::create("/dev/hidg0").await.unwrap();
         if secret == self.2 {
             for c in self.1[id][2].as_str().expect("Unknown JSON id").chars() {
-                let cx = (c as u8) - ('a' as u8) + 4;
+                let cx = get_key_codes(c);
                 gadgetFIle
-                    .write_all(vec![0, 0, 0, cx, 0, 0, 0, 0].as_slice())
+                    .write_all(vec![if cx.0 {2} else {0}, 0, 0, cx.1, 0, 0, 0, 0].as_slice())
                     .await;
                 gadgetFIle
                     .write_all(vec![0, 0, 0, 0, 0, 0, 0, 0].as_slice())
@@ -95,6 +241,7 @@ impl Mobius for MobiusServer {
 async fn spawn(fut: impl Future<Output = ()> + Send + 'static) {
     tokio::spawn(fut);
 }
+
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
